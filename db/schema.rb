31c087af33b6fce7556af4ca3199134b2181b6ce
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-25 16:36:39 -0700

    updated schema with link to post, created app.js and updated applications.scss

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150525210545) do
+ActiveRecord::Schema.define(version: 20150525225823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150525210545) do
     t.datetime "updated_at", null: false
     t.string   "picture"
     t.integer  "user_id"
+    t.string   "link"
   end
 
   create_table "users", force: :cascade do |t|
