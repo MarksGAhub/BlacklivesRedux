3051e281b99240c8e29bb3faa0cfd4c6a6f03c27
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-28 19:55:02 -0700

    Added button, forms and push to posts.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -6,7 +6,19 @@ angular.module('blacklives', [])
 '$scope',
 //  Below is communicating with controller and view
 function($scope){
-  $scope.test = 'Hello World!';
+  $scope.posts = [
+
+  {title: 'post 1'},
+  {title: 'post 2'},
+  {title: 'post 3'},
+  {title: 'post 4'}
+
+  ];
+
+
+$scope.addPost = function(){
+  $scope.posts.push({title: 'A new post!'});
+};
 
 }]);
 
