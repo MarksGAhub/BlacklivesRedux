5c81f4ec09bab255582e147f17a130b638620df6
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-29 15:37:45 -0700

    Added some styling and started creating a factory.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -2,6 +2,10 @@
 (function(){
 
 angular.module('blacklives', [])
+// The below factory is a Service - which is creating an object to organize and share data across the app.
+.factory('posts',[function(){
+
+}])
 .controller('MainCtrl', [
 '$scope',
 //  Below is communicating with controller and view
