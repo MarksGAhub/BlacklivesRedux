d79652a1139155ecba83d6b28558641f20b8d119
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-05-30 12:14:23 -0700

    updated app.js with factory and binding scope to posts

--- app/assets/javascripts/config/app.js
+++ app/assets/javascripts/config/app.js
@@ -1,5 +1,5 @@
 //IFFE
-//  This is creating dependencies for UI.router, templates and resources.
+
 
 (function(){
 angular.module('blacklives', [])
@@ -20,14 +20,14 @@ function($scope, posts){
 // Binding the $scope variable in our controller to the posts array in the Service(the .factory above)
   $scope.posts = posts.posts;
 
-  [
+  // [
 
-  {title: 'post 1'},
-  {title: 'post 2'},
-  {title: 'post 3'},
-  {title: 'post 4'}
+  // {title: 'post 1'},
+  // {title: 'post 2'},
+  // {title: 'post 3'},
+  // {title: 'post 4'}
 
-  ];
+  // ];
 // The below if statement is preventing a user from submitting a blank title.
 if(!$scope.title || $scope.title === '') { return; }
 // The addPost function is retrieving the title entered into the form
@@ -41,7 +41,7 @@ $scope.addPost = function(){
 })();
 
 
-
+//  This is creating dependencies for UI.router, templates and resources.
 // The below needs to be put in to create dependencies.
 // (function(){
 
