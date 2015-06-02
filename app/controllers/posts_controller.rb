63b486567afcc68fb52ab87948ff128fa140e943
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2015-06-02 09:52:53 -0700

    create the index, create and show action in postscontroller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,22 @@
 class PostsController < ApplicationController
+
+    def post_params
+        params.require(:post).permit(:title)
+    end
+
+
+    def index
+        respond_with Post.all
+    end
+
+
+    def create show
+        respond_with Post.find(params[:id])
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title)
+    end
+
 end
